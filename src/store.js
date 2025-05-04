27ddd3aeafd29d7a3a868e3049ace335fcd83b71
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-05-03 23:56:29 -0500

    feat: add function to update state

--- src/store.js
+++ src/store.js
@@ -3,3 +3,9 @@ function createStore(initialState = {}) {
   const listeners = new Set()
 
   const getState = () => ({ ...state })
+
+  const setState = (newState) => {
+    state = typeof newState === 'function' ? newState(state) : { ...state, ...newState }
+
+    listeners.forEach((listener) => listener(state))
+  }
