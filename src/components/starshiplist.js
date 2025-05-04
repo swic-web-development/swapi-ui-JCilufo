176aab59955b2f6fbf462af5f293ce23fc82885f
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-05-04 00:21:48 -0500

    feat: iterate through starships and put them on the card

--- src/components/starshiplist.js
+++ src/components/starshiplist.js
@@ -1,1 +1,9 @@
 import { StarshipCard } from './starshipcard'
+
+export function StarshipList(starships) {
+  return `
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+      ${starships.map((starship) => StarshipCard(starship)).join('')}
+    </div>
+  `
+}
