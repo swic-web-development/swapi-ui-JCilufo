1e1977a81e1a7aa2c3f58b208b2d1d3aac191251
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-05-04 00:16:56 -0500

    style: style the header component and export it

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+export function Header() {
+  return `
+      <header class="w-full bg-gray-800 p-4 mb-8">
+        <div class="container mx-auto">
+          <h1 class="text-3xl font-bold text-center">Star Wars Starships</h1>
+        </div>
+      </header>
+    `
+}
