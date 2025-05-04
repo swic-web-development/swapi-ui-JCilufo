eeb52817a194e5d80806cc7a83a432536a15bdd2
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-05-04 00:02:14 -0500

    style: add styles to app class name

--- src/main.js
+++ src/main.js
@@ -4,3 +4,5 @@ import { Header } from './components/header'
 import { StarshipList } from './components/starshiplist'
 import store from './store'
 
+const APP_CLASS_NAME =
+  'bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center'
