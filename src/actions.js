997e2aa4a4ee18b414b3fca799c8b1f586454907
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-05-04 00:14:21 -0500

    feat: fetch and await starship data from api

--- src/actions.js
+++ src/actions.js
@@ -2,3 +2,16 @@ import store from './store'
 
 export async function fetchStarships() {
   store.setState({ isLoading: true, error: null })
+
+  try {
+    const response = await fetch('https://swapi.tech/api/starships/')
+    const data = await response.json()
+
+    // Fetch detailed data for each starship
+    const starshipPromises = data.results.map(async (starship) => {
+      const detailResponse = await fetch(starship.url)
+      const detailData = await detailResponse.json()
+      return detailData.result.properties
+    })
+
+    const starshipsDetailed = await Promise.all(starshipPromises)
